e37bf87144446509b5c70785997d2897639893fc
Yair Churi <yair.churi@gmail.com>
2024-10-06 18:31:16 -0700

    finished generic api

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,32 +6,55 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
+  
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(200).json(books);
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  if (books[req.params.isbn]){
+    return res.status(200).json(books[req.params.isbn]);
+  }else{
+    return res.status(404).json({message: `Book ISBN ${req.params.isbn} not found.`});
+  }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const authorToSearch = req.params.author;
+  let booksByAuthor = {};
+  for (const key of Object.keys(books)){
+    if (books[key].author === authorToSearch){
+      booksByAuthor[key] = books[key];
+    }
+  }
+  if (Object.keys(booksByAuthor).length > 0){
+    return res.status(200).json(booksByAuthor);
+  }else{
+    return res.status(404).json({message: `Book authered by ${authorToSearch} not found.`});
+  }
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const bookTitle = req.params.title;
+  let bookDetails = {}
+  for (const key of Object.keys(books)){
+    if (books[key].title == bookTitle){
+      bookDetails[key] = books[key];
+    }
+  }
+  console.log(bookDetails);
+  if (Object.keys(bookDetails).length > 0){
+    return res.status(200).json(bookDetails);
+  }else{
+    return res.status(404).json({message: `Book with title ${bookTitle} not found.`});
+  }
 });
 
 //  Get book review
